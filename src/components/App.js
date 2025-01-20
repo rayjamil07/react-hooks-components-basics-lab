d1dddf28e30db76ac18d9a58e061067af0e15770
rayjamil <ryanwekesa019@gmail.com>
2025-01-20 14:46:54 +0300

    Adds mltiple files

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,22 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+      <p>The jorney begins, its a race against time, redeeming the times for the days are evil</p>
+    </div>
+  )
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home/>
+      <About/>
     </div>
   );
 }
